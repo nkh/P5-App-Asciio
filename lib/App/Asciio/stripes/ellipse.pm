df36e6bda0a35db0ff6ce991a1b03131a5cc8a68
QinQing <northisland2017@gmail.com>
2023-07-11 21:29:20 +0800

    ADDED: add border connection for ellipse and rhombus
    CHANGED: del make_unicode make_ascii
    because we have make_selection_unicode, Functional duplication

--- lib/App/Asciio/stripes/ellipse.pm
+++ lib/App/Asciio/stripes/ellipse.pm
@@ -721,6 +721,10 @@ elsif($x >= 0 && $x < $self->{WIDTH} && $y >= 0 && $y < $self->{HEIGHT})
 	{
 	return {X =>  $middle_width, Y => -1, NAME => 'to_be_optimized'} ;
 	}
+elsif($self->{ALLOW_BORDER_CONNECTION} && $x >= -1 && $x <= $self->{WIDTH} && $y >= -1 && $y <= $self->{HEIGHT})
+	{
+	return {X =>  $x, Y => $y, NAME => 'border'} ;
+	}
 else
 	{
 	return ;
@@ -842,11 +846,8 @@ sub get_box_type { my ($self) = @_ ; return($self->{BOX_TYPE})  ; }
 
 #-----------------------------------------------------------------------------
 
-sub allow_border_connection { ; }
-
-#-----------------------------------------------------------------------------
-
-sub is_border_connection_allowed { ; }
+sub allow_border_connection { my($self, $allow) = @_ ; $self->{ALLOW_BORDER_CONNECTION} = $allow ; }
+sub is_border_connection_allowed { my($self) = @_ ; return $self->{ALLOW_BORDER_CONNECTION} ; }
 
 #-----------------------------------------------------------------------------
 
