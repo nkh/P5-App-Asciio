207c17508b3f7a29cd6b29e7ba2f65b929dd41d5
Nadim Khemir <nadim.khemir@gmail.com>
2025-11-08 13:35:38 +0100

    FIXED: png exporter

--- setup/GTK/import_export/png.pl
+++ setup/GTK/import_export/png.pl
@@ -1,4 +1,7 @@
 
+use Cairo ;
+use List::Util qw(min max) ;
+
 register_import_export_handlers 
 	(
 	png => 
@@ -10,24 +13,26 @@ register_import_export_handlers
 
 sub export_png
 {
-# my ($self, $elements_to_save, $file)  = @_ ;
+my ($self, $elements_to_save, $file)  = @_ ;
 
-# if($self->{CREATE_BACKUP} && -e $file)
-# 	{
-# 	use File::Copy;
-# 	copy($file,"$file.bak") or die "export_pod: Copy failed while making backup copy: $!";		
-# 	}
+if($self->{CREATE_BACKUP} && -e $file)
+	{
+	use File::Copy;
+	copy($file,"$file.bak") or die "export_pod: Copy failed while making backup copy: $!" ;
+	}
 
-# my $alloc = $self->{widget}->allocation;
-# my $pixbuf = Gtk3::Gdk::Pixbuf->get_from_drawable
-# 			(
-# 			$self->{PIXMAP},
-# 			$self->{widget}->window->get_colormap,
-# 			0, 0,
-# 			0, 0,
-# 			$alloc->width, $alloc->height
-# 			);
-			
-# return $pixbuf->save($file, "png" );
+local $self->{BINDINGS_COMPLETION} = undef ;
+
+my ($font_character_width, $font_character_height) = $self->get_character_size($self->{FONT_FAMILY}, $self->{FONT_BINDINGS_SIZE}) ;
+
+my $width  = max( ( map { $_->{X} + 1 + $_->{EXTENTS}[2]} $elements_to_save->@* )) * $font_character_width ;
+my $height = max( ( map { $_->{Y} + 1 + $_->{EXTENTS}[3]} $elements_to_save->@* )) * $font_character_height ;
+
+my $surface = Cairo::ImageSurface->create('argb32', $width, $height);
+my $cr      = Cairo::Context->create($surface);
+
+App::Asciio::GTK::Asciio::expose_event($self->{widget}, $cr, $self) ;
+
+$surface->write_to_png("$file");
 }
 
