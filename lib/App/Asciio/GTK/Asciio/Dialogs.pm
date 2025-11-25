819504dacbe51ee73f4c2d6ebf60ae7fd1660ebe
QinQing <northisland2017@gmail.com>
2025-11-25 16:30:47 +0800

    FIXED: inline edit alignment error

--- lib/App/Asciio/GTK/Asciio/Dialogs.pm
+++ lib/App/Asciio/GTK/Asciio/Dialogs.pm
@@ -288,7 +288,7 @@ $text_width = max($text_width, 3) ;
 $text_heigh =max($text_heigh, 3) ;
 
 my ($character_width, $character_height) = $asciio->get_character_size() ;
-my ($root_x, $root_y) = $asciio->{root_window}->get_position();
+my ($root_x, $root_y) = $asciio->{root_window}->get_window()->get_origin() ;
 my ($v_value, $h_value) = ($asciio->{sc_window}->get_vadjustment()->get_value(), $asciio->{sc_window}->get_hadjustment()->get_value());
 
 
--- lib/App/Asciio/GTK/Asciio/stripes/editable_box2.pm
+++ lib/App/Asciio/GTK/Asciio/stripes/editable_box2.pm
@@ -162,7 +162,7 @@ my $final_width = max($title_width, $text_width) ;
 my $final_heigh = $text_heigh + $title_heigh + 1;
 
 my ($character_width, $character_height) = $asciio->get_character_size() ;
-my ($root_x, $root_y) = $asciio->{root_window}->get_position();
+my ($root_x, $root_y) = $asciio->{root_window}->get_window()->get_origin() ;
 my ($v_value, $h_value) = ($asciio->{sc_window}->get_vadjustment()->get_value(), $asciio->{sc_window}->get_hadjustment()->get_value());
 
 
