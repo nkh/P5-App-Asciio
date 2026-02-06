8542909d8b4620c38c99382c7153d20ad8e4d163
nkh <nkh@users.noreply.github.com>
2026-02-06 22:12:52 +0000

    deploy: 3debf71631ae043990fa8ccf52608c282ff67037

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a6e238fa.js',
+                path_to_root + 'searchindex-7ba5785c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
